e50794a3ee7964d9b5bfc7596ef5fb7c5eeeabfb
herman <hmcherman@gmail.com>
2016-05-04 12:00:20 +0800

    Add tracesource in LogUtility

--- src/Shared/LogUtility.cs
+++ src/Shared/LogUtility.cs
@@ -11,20 +11,24 @@ namespace Microsoft.Web.Redis
 {
     internal static class LogUtility
     {
+        private static readonly System.Diagnostics.TraceSource trace = new System.Diagnostics.TraceSource("Microsoft.Web.Redis");
         public static TextWriter logger = null;
 
         public static void LogWarning(string msg, params object[] args)
         {
+            trace.TraceEvent(System.Diagnostics.TraceEventType.Warning, 1, msg, args);
             Log("[Warning]", msg, args);
         }
 
         public static void LogError(string msg, params object[] args)
         {
+            trace.TraceEvent(System.Diagnostics.TraceEventType.Error, 2, msg, args);
             Log("[Error]", msg, args);
         }
 
         public static void LogInfo(string msg, params object[] args)
         {
+            trace.TraceEvent(System.Diagnostics.TraceEventType.Information, 3, msg, args);
             Log("[Info]", msg, args);
         }
 
